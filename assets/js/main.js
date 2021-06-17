d3f6476519746145f3cc205fb852394c97ca7188
adarshnjena <adarshnjena@gmail.com>
2021-06-17 11:00:20 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 1000,
+    duration: 1300,
     reset: true
 });
 
